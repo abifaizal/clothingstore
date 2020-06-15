3c63ea6141257294e6261ccf8dfb220ad6462c19
Faizal Nur Abidin <43515678+abifaizal@users.noreply.github.com>
2020-06-15 23:05:29 +0700

    Add files via upload

--- administrator/login.php
+++ administrator/login.php
@@ -9,7 +9,7 @@
 	<head>
 	  <meta charset="utf-8">
 	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	  <title>Black Sabath | Log in</title>
+	  <title>Black Shadow | Log in</title>
 	  <!-- Tell the browser to be responsive to screen width -->
 	  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	  <!-- Bootstrap 3.3.7 -->
@@ -38,7 +38,7 @@
 	<body class="hold-transition login-page">
 		<div class="login-box">
 		  <div class="login-logo">
-		    <a href="#"><b>Black Sabath</b>Distro</a>
+		    <a href="#"><b>Black Shadow</b> Distro</a>
 		  </div>
 		  <!-- /.login-logo -->
 		  <div class="login-box-body">
