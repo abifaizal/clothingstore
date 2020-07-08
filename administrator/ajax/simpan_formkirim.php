fb4f1192b986160219d84946760197cc75fcbeba
Faizal Nur Abidin <43515678+abifaizal@users.noreply.github.com>
2020-07-08 23:27:34 +0700

    Add files via upload

--- administrator/ajax/simpan_formkirim.php
+++ administrator/ajax/simpan_formkirim.php
@@ -10,7 +10,7 @@
 	$lama_pengiriman = $_POST['lama_pengiriman'];
 	$catatan_kirim = $_POST['catatan_kirim'];
 
-	$query_formkirim = "INSERT INTO tbl_datapengiriman VALUES ('', '$no_resi', '$jasa_kirim', '$tgl_kirim', '$lama_pengiriman', '$catatan_kirim', CURDATE(), '$no_penjualan', '$id_pgw')";
+	$query_formkirim = "INSERT INTO tbl_datapengiriman (no_resi, jasa_kirim, tgl_kirim, lama_kirim, catatan_kirim, tgl_record, no_penjualan, id_pgw) VALUES ('$no_resi', '$jasa_kirim', '$tgl_kirim', '$lama_pengiriman', '$catatan_kirim', CURDATE(), '$no_penjualan', '$id_pgw')";
 	mysqli_query($conn, $query_formkirim) or die ($conn->error);
 
 	$query_ubahverifikasi = "UPDATE tbl_penjualan SET status_penjualan = 'Dikirim' WHERE no_penjualan = '$no_penjualan'";
