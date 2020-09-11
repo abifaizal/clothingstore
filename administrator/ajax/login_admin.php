e96cf448215ba9efb8077f434900de9d591ac3ee
Faizal Nur Abidin <43515678+abifaizal@users.noreply.github.com>
2020-09-11 23:57:30 +0700

    Update login_admin.php
    - add pengkondisian jika status pegawai 'aktif'

--- administrator/ajax/login_admin.php
+++ administrator/ajax/login_admin.php
@@ -5,7 +5,7 @@
 	$username_pgw = $_POST['username_pgw'];
   $password_pgw = $_POST['password_pgw'];
 
-	$query = "SELECT * FROM tbl_pegawai WHERE username_pgw = '$username_pgw' AND password_pgw = '$password_pgw'";
+	$query = "SELECT * FROM tbl_pegawai WHERE username_pgw = '$username_pgw' AND password_pgw = '$password_pgw' AND aktif_pgw = 'Aktif'";
 	$sql = mysqli_query($conn, $query) or die ($conn->error);
 	$data = mysqli_fetch_array($sql);
 	if(mysqli_num_rows($sql) > 0) {
